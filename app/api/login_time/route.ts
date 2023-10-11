b0512d7b716a8ee8b2c3c800decfc2ca7c201189
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-10-11 15:49:51 +0800

    add average login session to login_time API

--- app/api/login_time/route.ts
+++ app/api/login_time/route.ts
@@ -8,7 +8,7 @@ function getNDaysAgoDate(n: number) {
 }
 
 
-function getTimePassedInSeconds(begin, end) {
+function getTimePassedInSeconds(begin: string, end: string) {
   const date1 = new Date(begin);
 
   let date2: Date;
@@ -33,9 +33,6 @@ export async function GET(
 ) {
   const searchParams = req.nextUrl.searchParams
   const topN = parseInt(searchParams.get('n'))
-  if (!topN) {
-    return NextResponse.json({'error': 'Invalid params'}, { status: 400 });
-  }
   const accessToken = await getAccessToken()
   let page = 1;
   const beginTime = getNDaysAgoDate(7)
@@ -44,16 +41,20 @@ export async function GET(
   if (!allLoginSessions.ok) {
     return NextResponse.json({'error': 'Error'}, { status: 400 });
   }
-  let loginSessionsJson: Object[] = await allLoginSessions.json()
-  let usersLoginTime: Object[] = []
+  let loginSessionsJson: any[] = await allLoginSessions.json()
+  let usersLoginTime: any[] = []
 
   const allStudentsInfo = await getStudentsInfo()
+
+  let totalLoginTime = 0;
+  let totalSessionsCount = 0;
   
   while (loginSessionsJson.length !== 0) {
     loginSessionsJson.forEach((sessionInfo) => {
+      let sessionTime = getTimePassedInSeconds(sessionInfo.begin_at, sessionInfo.end_at);
       const userLoginInfo = usersLoginTime.find((user) => user.id === sessionInfo.user.id)
       if (userLoginInfo === undefined) {
-        let studentInfo = allStudentsInfo.find(info => info.login === sessionInfo.user.login);
+        let studentInfo = allStudentsInfo.find((info: any) => info.login === sessionInfo.user.login);
         usersLoginTime.push({
           id: sessionInfo.user.id,
           login: sessionInfo.user.login,
@@ -66,11 +67,14 @@ export async function GET(
           cp_batch_year: studentInfo.cp_batch_year,
           cp_batch_month: studentInfo.cp_batch_month,
           level: studentInfo.cp_level || studentInfo.ps_level,
-          login_time: getTimePassedInSeconds(sessionInfo.begin_at, sessionInfo.end_at),
+          login_time: sessionTime,
         })
       } else {
-        userLoginInfo.login_time += getTimePassedInSeconds(sessionInfo.begin_at, sessionInfo.end_at);
+        userLoginInfo.login_time += sessionTime;
       }
+
+      totalLoginTime += sessionTime;
+      totalSessionsCount++;
     })
     page++;
     allLoginSessions = await fetch(`https://api.intra.42.fr/v2/campus/34/locations?page[size]=100&page[number]=${page}&range[begin_at]=${beginTime},${endTime}&access_token=${accessToken}`)
@@ -85,7 +89,11 @@ export async function GET(
     return user2.login_time - user1.login_time
   });
 
-  const topUsersLoginTime = usersLoginTime.slice(0, topN);
+  if (topN === topN) {
+    usersLoginTime = usersLoginTime.slice(0, topN);
+  }
 
-  return NextResponse.json(topUsersLoginTime, { status: 200 });
+  let averageSessionTimeInHours = Math.round(totalLoginTime / 3600 / totalSessionsCount * 10) / 10
+
+  return NextResponse.json({top_zombies: usersLoginTime, average_session_time: averageSessionTimeInHours}, { status: 200 });
 }
